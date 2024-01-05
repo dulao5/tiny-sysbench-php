bbb23d65a654fd928797075a534777615b85a22d
Du Zhigang <dulao5@gmail.com>
2024-01-05 05:44:04 +0000

    チューニング

--- app/public/index.php
+++ app/public/index.php
@@ -6,8 +6,11 @@ define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : 'test');
 define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : 'root');
 define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '');
 
+define('DB_PERSISTENT_CONN', getenv('DB_PERSISTENT_CONN') ? getenv('DB_PERSISTENT_CONN') : false);
+
 define('SYSBENCH_MULTI_DB', getenv('SYSBENCH_MULTI_DB') ? getenv('SYSBENCH_MULTI_DB') : false);
 define('OLTP_DB_COUNT', getenv('OLTP_DB_COUNT') ? getenv('OLTP_DB_COUNT') : 50);
+
 define('OLTP_TABLE_COUNT', getenv('OLTP_TABLE_COUNT') ? getenv('OLTP_TABLE_COUNT') : 100);
 define('OLTP_TABLE_SIZE', getenv('OLTP_TABLE_SIZE') ? getenv('OLTP_TABLE_SIZE') : 10000);
 
@@ -23,10 +26,17 @@ define('OLTP_RANGE_SIZE', 100);
 define('SBTEST_COLUMN_C_LENGTH', 120);
 define('SBTEST_COLUMN_PAD_LENGTH', 60);
 
+$pdoAttrs = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ];
+if (DB_PERSISTENT_CONN) {
+	$pdoAttrs[ PDO::ATTR_PERSISTENT ] = true;
+}
+
+$dbName = "test";
 if (SYSBENCH_MULTI_DB) {
     $dbId = mt_rand(0, OLTP_DB_COUNT-1);
     $dbName = sprintf("sbtest%03d", $dbId);
-    define('PDO_DSN', 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.$dbName);
+    // define('PDO_DSN', 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.$dbName);
+    define('PDO_DSN', 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname=test');
     echo("connecting to the database: " . PDO_DSN);
 } else {
     define('PDO_DSN', 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME);
@@ -34,8 +44,7 @@ if (SYSBENCH_MULTI_DB) {
 
 // 创建 PDO 对象
 try {
-    $pdo = new PDO(PDO_DSN, DB_USER, DB_PASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(PDO_DSN, DB_USER, DB_PASSWORD, $pdoAttrs);
 } catch (PDOException $e) {
     die("Error connecting to the database: " . $e->getMessage());
 }
@@ -43,13 +52,25 @@ try {
 // query 函数
 function query($pdo, $sql, $params = []) {
     $stmt = $pdo->prepare($sql);
-    $stmt->execute($params);
+
+    foreach ($params as $i => $param) {
+        if (is_int($param)) {
+            $stmt->bindValue($i + 1, $param, PDO::PARAM_INT);
+        } else {
+            $stmt->bindValue($i + 1, $param);
+        }
+    }
+
+    $r = $stmt->execute();
+
     echo "$sql , [ " . join(' , ', $params) . " ]\n";
     // 如果是 SELECT 查询，读取并废弃结果集
     if (strpos(strtoupper($sql), 'SELECT') === 0) {
-        while ($stmt->fetch()) {
-            //print_r($row);
+        while ($row = $stmt->fetch()) {
+            // print_r($row);
         }
+    } else {
+        echo "result : $r\n";
     }
 }
 
@@ -81,7 +102,11 @@ function getRandomString($length = 60) {
 
 // 执行 OLTP 场景查询
 function executeOltpScenarios($pdo) {
+    global $dbName;
     $randomTableName = getRandomTableName();
+    query($pdo, "SET SESSION wait_timeout = 60");
+    query($pdo, "USE $dbName");
+    query($pdo, "BEGIN");
 
     // Point Selects
     for ($i = 0; $i < OLTP_POINT_SELECTS; $i++) {
@@ -138,6 +163,7 @@ function executeOltpScenarios($pdo) {
               getRandomString(SBTEST_COLUMN_C_LENGTH),
               getRandomString(SBTEST_COLUMN_PAD_LENGTH)
          ]);
+    query($pdo, "COMMIT");
 }
 
 // 执行 OLTP 测试
--- gatling/scenario/base.scala
+++ gatling/scenario/base.scala
@@ -7,6 +7,8 @@ import io.gatling.http.Predef._
 
 class BasicSimulation extends Simulation {
 
+  var maxClients = 25
+
   val httpProtocol = http
     .baseUrl("http://nginx")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -14,6 +16,8 @@ class BasicSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+    .maxConnectionsPerHost(maxClients)
+    .shareConnections
 
   val scn = scenario("BasicSimulation")
     .exec(
@@ -24,7 +28,7 @@ class BasicSimulation extends Simulation {
 
   setUp(
     scn.inject(
-        constantConcurrentUsers(100) during(3600*8 seconds)
+        constantConcurrentUsers(maxClients) during(300 seconds)
     )
   ).protocols(httpProtocol)
 }
